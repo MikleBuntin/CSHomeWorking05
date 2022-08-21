e77c03442964527debaefc798ac47789f47b77e0
MikleBuntin <BuntinMA@mail.ru>
2022-08-21 17:49:43 +0300

    Решил задачу 34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int n = new Random().Next(2, 20);
+int[] array = new int[n];
+
+void FloodArray(int[] array)
+{
+    int count = 0;
+    while(count < array.Length)
+    {
+        array[count] = new Random().Next(100, 1000);
+        count++;
+    }
+}
+
+int ArrayChetNumber(int[] array)
+{
+    int chetNumber = 0;
+    int count = 0;
+    while(count < array.Length)
+    {
+        if(array[count] % 2 == 0) chetNumber++;
+        count++;
+    }
+    return chetNumber;
+}
+
+void PrintArray(int[] array)
+{
+    int count = 0;
+    Console.Write($"[");
+    while(count < array.Length -1)
+    {
+        Console.Write(array[count] + $", ");
+        count++;
+    }
+    Console.WriteLine(array[count] + $"]");
+}
+
+FloodArray(array);
+PrintArray(array);
+Console.WriteLine("Всего элементов: " + n);
+int chetNumber = ArrayChetNumber(array);
+Console.WriteLine("Количество чётных элементов массива: " + chetNumber);
